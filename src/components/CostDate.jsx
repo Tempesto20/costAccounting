4a5fcb35afbe6c8fa3d31e7c57bbd6fa4529b6ae
Tempesto20 <Bro74rus1@mail.ru>
2022-08-14 19:16:32 +0300

    просы через нескоько файлов

--- src/components/CostDate.jsx
+++ src/components/CostDate.jsx
@@ -12,6 +12,7 @@ function CostDate(props) {
         <div className='cost-date__month'> {month}  </div>
         <div className='cost-date__year'> {year}  </div>
         <div className='cost-date__day'> {day}  </div>
+        
     </div>
   );
 }
