d068742412af7bd5981c270fb7035c571e822c71
Tempesto20 <Bro74rus1@mail.ru>
2022-08-19 13:44:19 +0300

    use useState - const [description, setDescription] = useState(props.description);

--- src/components/Costs/CostItem.jsx
+++ src/components/Costs/CostItem.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import CostDate from './CostDate';
 import './CostItem.css';
 
 
 const CostItem = (props) => {
+  
+  const [description, setDescription] = useState(props.description);
 
+
+  const chengeDescriptioHamdler= ()=>{
+    setDescription('new text');
+  }
 
 
   return (
@@ -13,10 +19,11 @@ const CostItem = (props) => {
       <CostDate date={props.date}/>
  
       <div className='cost-item__description'>
-        
-        <h2> {props.description} </h2>
+
+        <h2> {description} </h2>
         <div className='cost-item__price'> {props.amount} </div>
       </div>
+      <button onClick={chengeDescriptioHamdler}>Chenge description</button>
     </Card>
   );
 }
