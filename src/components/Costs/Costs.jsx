eeb30b40d2c1366bcef24123f57dcdcccb2d78ad
Tempesto20 <Bro74rus1@mail.ru>
2022-08-23 13:47:39 +0300

    revork Costs формирование динамического массива из app {           props.costs.map(  (cost) =>(             <CostItem               date={cost.date}               description={cost.description}               amount={cost.amount}             />           )           )           }

--- src/components/Costs/Costs.jsx
+++ src/components/Costs/Costs.jsx
@@ -26,23 +26,37 @@ return (
           onChangeYear = {yearChangeHandler}
         />
 
-        <CostItem 
+          { 
+          props.costs.map(  (cost) =>(
+            <CostItem 
+              date={cost.date}
+              description={cost.description}
+              amount={cost.amount}
+            />
+          )
+          )   
+          }
+
+
+          {/*
+            <CostItem 
             date={props.costs[0].date}
             description={props.costs[0].description}
             amount={props.costs[0].amount}
-        />
+            />
 
-        <CostItem 
-          date={props.costs[1].date}
-          description={props.costs[1].description}
-          amount={props.costs[1].amount}
-        />
-        
-        <CostItem 
-          date={props.costs[2].date}
-          description={props.costs[2].description}
-          amount={props.costs[2].amount}
-        />
+            <CostItem 
+              date={props.costs[1].date}
+              description={props.costs[1].description}
+              amount={props.costs[1].amount}
+            />
+            
+            <CostItem 
+              date={props.costs[2].date}
+              description={props.costs[2].description}
+              amount={props.costs[2].amount}
+            />
+          */}
     
       </Card>
     
